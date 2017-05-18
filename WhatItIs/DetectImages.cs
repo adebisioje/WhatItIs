1570200b7974db227693c9f23f41f0803427506b
adebisioje <bisioje@gmail.com>
2017-05-17 20:02:07 -0400

    A bot that tells you what is in an image
    A bot that tells you what is in an image

---
+++ WhatItIs/DetectImages.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WhatItIs
+{
+    public class DetectImages
+    {
+
+        private static readonly VisualFeature[] VisualFeatures = { VisualFeature.Description };
+
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        public static async Task<string> DetectImageAsync(Stream stream)
+        {
+            var response = await MakeAnalysisRequest(stream);
+            return response;
+        }
+
+        static async Task<string> MakeAnalysisRequest(Stream stream)
+        {
+            var client = new VisionServiceClient("425b34f8517c404fa4d6b0b1b0957292");
+            var result = await client.AnalyzeImageAsync(stream, VisualFeatures);
+            return ProcessAnalysisResult(result);
+
+    }
+        private static string ProcessAnalysisResult(AnalysisResult result)
+
+        {
+
+            string message = result?.Description?.Captions.FirstOrDefault()?.Text;
+            return string.IsNullOrEmpty(message) ?
+
+                        "Couldn't find a caption for this one" :
+
+                        "I think it's " + message;
+
+        }
+
+        static async Task<string> MakeAnalysisRequest2(byte[] byteDat)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "2474b217e55942b69797d2f67422bc76");
+
+            // Request parameters
+            queryString["visualFeatures"] = "Categories";
+            queryString["details"] = "{string}";
+            queryString["language"] = "en";
+            var uri = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + queryString;
+
+            HttpResponseMessage response;
+
+            // Request body
+             byte [] byteData = Encoding.UTF8.GetBytes("{body}");
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+            }
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
+    
+        
+
+    
+
+
+}
---
+++ WhatItIs/Dialogs/RootDialog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Linq;
+namespace WhatItIs.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+
+            if (message.Attachments != null && message.Attachments.Any())
+            {
+                var attachment = message.Attachments.First();
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
+                    if ((message.ChannelId.Equals("skype", StringComparison.InvariantCultureIgnoreCase) || message.ChannelId.Equals("msteams", StringComparison.InvariantCultureIgnoreCase))
+                        && new Uri(attachment.ContentUrl).Host.EndsWith("skype.com"))
+                    {
+                        var token = await new MicrosoftAppCredentials().GetTokenAsync();
+                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    }
+
+                    var responseMessage = await httpClient.GetAsync(attachment.ContentUrl);
+
+                    var contentLenghtBytes = responseMessage.Content.Headers.ContentLength;
+
+                  
+
+
+                    string detectedIMage =  await DetectImages.DetectImageAsync(await responseMessage.Content.ReadAsStreamAsync());
+                    //await context.PostAsync($"Attachment of {attachment.ContentType} type and size of {contentLenghtBytes} bytes received.");
+                    await context.PostAsync($"{detectedIMage}");
+                }
+            }
+            else
+            {
+                await context.PostAsync("Hi there! I'm a bot created to tell you what is in the image you uploaded. Go ahead and upload an image!");
+            }
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+    }
+}
